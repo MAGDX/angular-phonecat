1735082b13e41e3a59dd78710fcbc1485cee38d0
MAGDX <magnum_dx@hotmail.com>
2019-10-23 13:48:50 +0200

    Empezando tarea boton Comprar

--- app/phone-card/phone-card.component.js
+++ app/phone-card/phone-card.component.js
@@ -2,16 +2,29 @@
 
 // Register `phoneComparador` component, along with its associated controller and template
 angular.
-  module('phoneCard').
-  component('phoneCard', {
-    templateUrl: 'phone-card/phone-card.template.html',
-    bindings: {
-      phone1: '=',
-      phone2: '='
-    },
-    controller: ['Phone',
-      function PhoneCardController(Phone) {
-        console.trace('PhoneCardController');
-      }
-    ]
-  });
+module('phoneCard').
+component('phoneCard', {
+  templateUrl: 'phone-card/phone-card.template.html',
+  bindings: {
+    phone1: '=',
+    phone2: '='
+  },
+  controller: ['Phone',
+    function PhoneCardController(Phone) {
+      console.trace('PhoneCardController');
+    }
+  ]
+}).
+directive('amComparator', function () {
+  var directiveDefinitionObject = {
+    restrict: "AE",
+    replace: false,
+    template: "<span class=\"label label-{{ (v1 >= v2)?'success':'danger' }}\">{{(v1 >= v2)?'+':''}} {{v1 - v2}} MB</span>",
+    scope: {
+      v1: "=",
+      v2: "="
+    }
+  }
+
+  return directiveDefinitionObject;
+});
